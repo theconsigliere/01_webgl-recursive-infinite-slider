2a4a0a60b0bfe4341b58eaf4bb09d2c79ff1aba5
Dirty Martini <dirtymartini@maxwells-mbp-2.home>
2024-01-03 22:08:20 +0000

    images on slider

--- js/app.js
+++ js/app.js
@@ -3,6 +3,18 @@ import fragment from "./shader/fragment.glsl"
 import vertex from "./shader/vertex.glsl"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
+// images
+import img1 from "../img/1.jpg"
+import img2 from "../img/2.jpg"
+import img3 from "../img/3.jpg"
+import img4 from "../img/4.jpg"
+import img5 from "../img/5.jpg"
+import img6 from "../img/6.jpg"
+import img7 from "../img/7.jpg"
+import img8 from "../img/8.jpg"
+import img9 from "../img/9.jpg"
+import img10 from "../img/10.jpg"
+
 export default class Sketch {
   constructor(options) {
     this.scene = new THREE.Scene()
@@ -13,8 +25,23 @@ export default class Sketch {
     this.renderer = new THREE.WebGLRenderer()
     this.renderer.setPixelRatio(window.devicePixelRatio)
     this.renderer.setSize(this.width, this.height)
-    this.renderer.setClearColor(0xeeeeee, 1)
+    this.renderer.setClearColor(0x000000, 1)
     this.renderer.outputEncoding = THREE.sRGBEncoding
+
+    //images
+    this.imgs = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10]
+    console.log(this.imgs)
+
+    //scroll
+    this.scroll = 0
+    this.scrollTarget = 0
+    this.currentScroll = 0
+
+    //slider
+    this.sliderMargin = 1.1
+    this.sliderLength = 10
+    this.sliderMeshes = []
+    this.sliderWidth = this.sliderMargin * this.sliderLength
 
     this.container.appendChild(this.renderer.domElement)
 
@@ -29,7 +56,7 @@ export default class Sketch {
     // var aspect = window.innerWidth / window.innerHeight;
     // this.camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
     this.camera.position.set(0, 0, 2)
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    // this.controls = new OrbitControls(this.camera, this.renderer.domElement)
     this.time = 0
 
     this.isPlaying = true
@@ -56,7 +83,9 @@ export default class Sketch {
   }
 
   scrollEvent() {
-    document.addEventListener("mousewheel", (e) => {})
+    document.addEventListener("mousewheel", (e) => {
+      this.scrollTarget = e.wheelDeltaY * 0.3
+    })
   }
 
   resize() {
@@ -90,16 +119,18 @@ export default class Sketch {
     this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1)
 
     // create slider loop
-    this.sliderLength = 10
-    this.sliderMeshes = []
+
     for (let i = 0; i < this.sliderLength; i++) {
-      let mesh = new THREE.Mesh(this.geometry, this.material)
+      let mesh = new THREE.Mesh(
+        this.geometry,
+        new THREE.MeshBasicMaterial({
+          // texture
+          map: new THREE.TextureLoader().load(this.imgs[i]),
+        })
+      )
       this.sliderMeshes.push({ mesh, index: i })
       this.scene.add(mesh)
     }
-
-    this.plane = new THREE.Mesh(this.geometry, this.material)
-    this.scene.add(this.plane)
   }
 
   stop() {
@@ -114,13 +145,26 @@ export default class Sketch {
   }
 
   updateMeshes() {
-    this.sliderMeshes.map((item) => (item.mesh.position.x = 1.1 * item.index))
+    this.sliderMeshes.map((item) => {
+      item.mesh.position.x =
+        ((this.sliderMargin * item.index +
+          this.currentScroll +
+          4209 * this.sliderWidth) %
+          this.sliderWidth) -
+        this.sliderMargin * 2
+
+      // console.log(item.index, item.mesh.position.x)
+    })
   }
 
   render() {
     if (!this.isPlaying) return
     this.time += 0.05
     // slider position
+    this.scroll += (this.scrollTarget - this.scroll) * 0.1
+    this.scroll *= 0.9
+    this.scrollTarget *= 0.9
+    this.currentScroll += this.scroll * 0.01
     this.updateMeshes()
     this.material.uniforms.time.value = this.time
     requestAnimationFrame(this.render.bind(this))
